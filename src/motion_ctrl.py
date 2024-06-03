b88852729ee2e3788004825b4b28717045af8699
erinakin <erinakinjide16@gmail.com>
2024-06-03 17:18:40 -0400

    Reorganizing scripts and files for navigation ease

--- src/motion_ctrl.py
+++ src/motion_ctrl.py
@@ -1,5 +1,5 @@
-from newportxps.newportxps import NewportXPS
-from newportxps.XPS_C8_drivers import XPSException
+from src.newportxps import NewportXPS
+from src.XPS_C8_drivers import XPSException
 from typing import Any, Literal
 
 parameters = Literal["Velocity", "Position"]
