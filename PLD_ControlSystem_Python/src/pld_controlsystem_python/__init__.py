d2c033a847dc70a69922e839a0470687489854c1
erinakin <erinakinjide16@gmail.com>
2024-06-06 15:57:21 -0400

    added import for attenuator as a potential solve for an earlier error with pytest

--- PLD_ControlSystem_Python/src/pld_controlsystem_python/__init__.py
+++ PLD_ControlSystem_Python/src/pld_controlsystem_python/__init__.py
@@ -1,5 +1,6 @@
 import sys
 from .newportxps import NewportXPS
+from .attenuator_ctrl import AttenuatorControls
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
